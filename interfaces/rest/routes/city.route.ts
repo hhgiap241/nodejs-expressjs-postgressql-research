a53cbe836f9cf985cfe4176639803a1076464265
hoanghuugiap241 <hoanghuugiap241@gmail.com>
2022-12-07 13:26:54 +0700

    Add generic schema to validate the request body before insert

--- interfaces/rest/routes/city.route.ts
+++ interfaces/rest/routes/city.route.ts
@@ -1,5 +1,6 @@
 import cityController from "../../../controllers/city.controller";
 import {makeRoute, Route} from "./route.factory";
+import citySchema from "../../../schemas/city.schema.json";
 
-const cityRoute: Route<any> = makeRoute( cityController);
+const cityRoute: Route<any> = makeRoute(cityController, citySchema);
 export default cityRoute.router;
--- interfaces/rest/routes/route.factory.ts
+++ interfaces/rest/routes/route.factory.ts
@@ -1,14 +1,15 @@
 import {Controller} from "../../../controllers/controller.factory";
 import {Router, Request, Response} from "express";
+import validate from "../../../middleware/validate.middleware";
+
 
 interface Route<T> {
   controller: Controller<T>,
   router: Router
 }
 
-const makeRoute = <T>( controller: Controller<T>): Route<T> => {
+const makeRoute = <T>(controller: Controller<T>, schema: any): Route<T> => {
   const route: Route<T> = {
-
     controller: controller,
     router: Router()
   }
@@ -20,7 +21,7 @@ const makeRoute = <T>( controller: Controller<T>): Route<T> => {
     const item = await route.controller.getItemById(req.params.id);
     res.status(200).json(item);
   });
-  route.router.post('/', async (req: Request, res: Response) => {
+  route.router.post('/', validate(schema), async (req: Request, res: Response) => {
     const item = await route.controller.insertItem(req.body);
     res.status(201).json(item);
   });

7a5e27cd27e142990008c0183e4df13624d1e563
hoanghuugiap241 <hoanghuugiap241@gmail.com>
2022-12-07 11:55:53 +0700

    Remove base path from route factory

--- interfaces/rest/routes/city.route.ts
+++ interfaces/rest/routes/city.route.ts
@@ -1,5 +1,5 @@
 import cityController from "../../../controllers/city.controller";
 import {makeRoute, Route} from "./route.factory";
 
-const cityRoute: Route<any> = makeRoute('/api/v1/cities', cityController);
+const cityRoute: Route<any> = makeRoute( cityController);
 export default cityRoute.router;
